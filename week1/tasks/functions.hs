25223686e124a2c55bebca0880e2754c4b0b6749
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-06-16 20:00:58 +0300

    Bugfixes

--- week1/tasks/functions.hs
+++ week1/tasks/functions.hs
@@ -1,5 +1,5 @@
 even' :: Integer -> Bool
-even' number = if number `mod` 2 == 0 then True else False
+even' number = number `mod` 2 == 0
 
 odd' :: Integer -> Bool
 odd' number = not (even' number)
@@ -14,7 +14,7 @@ rad2deg :: Double -> Double
 rad2deg rad = rad * 180 / pi
 
 isTriangle :: Integer -> Integer -> Integer -> Bool
-isTriangle a b c = if a == b && b == c then True else False
+isTriangle a b c = a > 0 && b > 0 && c > 0 && (a + b) > c && (b + c) > a && (a + c) > b
 
 perimeter :: Double -> Double -> Double -> Double
 perimeter a b c = a + b + c
@@ -23,7 +23,13 @@ area :: Double -> Double -> Double -> Double
 area a b c = sqrt ((perimeter a b c) / 2 * ((perimeter a b c) / 2 -a)  * ((perimeter a b c) / 2 -b) * ((perimeter a b c) / 2 - c))
 
 calculate :: Char -> Integer -> Integer -> Integer
-calculate operator a b = if operator == '+' then a + b else (if operator == '-' then a - b else (if operator == '*' then a * b else error "not supported"))
+calculate operator a b = if operator == '+' 
+                         then a + b 
+                         else (if operator == '-' 
+                               then a - b 
+                               else (if operator == '*' 
+                                     then a * b 
+                                     else error "not supported"))
 
 convert :: String -> String -> Float -> Float
 convert cur1 cur2 amount

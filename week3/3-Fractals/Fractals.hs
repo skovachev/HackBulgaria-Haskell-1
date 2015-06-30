01651d8634861f99dd258e43a88a6074e0df19ad
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-06-30 20:44:48 +0300

    Added week 3 | task 3 - Fractals

--- week3/3-Fractals/Fractals.hs
+++ week3/3-Fractals/Fractals.hs
@@ -20,21 +20,32 @@ screen = (600, 600)
 
 -- The part of the fractal we want to see
 window :: (Coordinate, Coordinate)
-window = undefined
+window = (top_left, bottom_right)
+    where top_left = (-1.9, 1.3)
+          bottom_right = (0.7, -1.3)
 
 -- Mapping a pixel to a coordinate
 pixelToCoordinate :: Int -> Int -> (Coordinate, Coordinate) -> Coordinate
-pixelToCoordinate x y ((minx, maxy), (maxx, miny)) = undefined
+pixelToCoordinate x y ((minx, maxy), (maxx, miny)) = (x_coord, y_coord)
+    where x_coord = (fromIntegral x / (fst screen)) * width - half_width
+          y_coord = (fromIntegral y / (snd screen)) * height - half_height
+          width = abs(minx) + abs(maxx)
+          height = abs(miny) + abs(maxy)
+          half_width = width / 2
+          half_height = height / 2
 
 -- Implement the formula for Mandelbrot's fractal
 mandelbrot :: Point -- Coordinate for calculation
            -> Point -- Current z value
            -> Int   -- Number of current iteration
            -> Int   -- Iterations before diverging
-mandelbrot c z iter = undefined
+mandelbrot c z iter | iter > maxIterations = 0
+                    | magnitude(zNext) > 2 = iter
+                    | otherwise = mandelbrot c zNext (iter+1)
+    where zNext = z**2 + c
 
 -- Returning the number of iterations needed for given
 -- point to diverge
 drawMandelbrot :: Int -> Int -> Int
 drawMandelbrot x y = mandelbrot (x' :+ y') (0 :+ 0) 0
-  where (x', y') = pixelToCoordinate x y window
+    where (x', y') = pixelToCoordinate x y window

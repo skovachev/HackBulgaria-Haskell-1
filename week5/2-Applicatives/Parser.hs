d0f672bb2dae9a7ecc9d9110aa95f24c03abe56a
Stefan Kovachev <stefan.kovachev@gmail.com>
2015-07-21 21:40:50 +0300

    Finished parser task of week5

--- week5/2-Applicatives/Parser.hs
+++ week5/2-Applicatives/Parser.hs
@@ -1,38 +1,76 @@
 module Parser where
 
-
 import Control.Applicative
+import Control.Arrow (first)
+
 
 newtype Parser a =
   Parser { parse :: String -> Maybe (a, String) }
 
+-- parse parserInstance = parserFunc
+
 instance Functor Parser where
-  fmap f (Parser p) = undefined
+  -- fmap f (Parser parseFunc) = Parser (\str -> applyFunc f (parseFunc str))
+  --   where applyFunc f (a, b) = Just (f a, b)
+
+  fmap f (Parser parseFunc) = Parser $ \str -> case parseFunc str of
+                                               Just (x, restStr) -> Just (f x, restStr)
+                                               Nothing           -> Nothing
 
 instance Applicative Parser where
-  pure  = undefined
-  (<*>) = undefined
+  -- returns a parser whose func is 'the creation of a Maybe, unfinished tuple with "a" as its 1st element'
+  pure a = Parser $ Just . (,) a
+
+  -- return a parser by extracting the function (in place of 'a') from parserA
+  -- and applying it to the 'a' of parserB if it's not Nothing
+  Parser p <*> Parser k = Parser $ \s -> case p s of
+                                         Just (func, restStr) -> first func <$> k restStr
+                                         Nothing              -> Nothing
 
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy = undefined
+satisfy f = Parser (\(x:xs) -> if f x then Just (x, xs) else Nothing)
 
 char :: Char -> Parser Char
-char = undefined
+char c = satisfy (==c)
 
 openingBrace :: Parser Char
-openingBrace = undefined
+-- openingBrace = Parser f
+--     where f (x:y:xs) | x == '(' = Just (y, xs)
+--                      | otherwise = Nothing
+--           f _        = Nothing
+openingBrace = char '('
 
 closingBrace :: Parser Char
-closingBrace = undefined
+-- closingBrace = Parser f
+--     where f (x:xs) | x == ')' = Just (x, xs)
+--                    | otherwise = Nothing
+--           f _      = Nothing
+closingBrace = char ')'
 
 inBraces :: Parser a -> Parser a
 inBraces p = openingBrace *> p <* closingBrace
 
 instance Alternative Parser where
-  empty = undefined
-  (<|>) = undefined
+  -- return parser whose parse function returns empty -> Nothing
+  empty = Parser $ \str -> Nothing
+
+  -- return a parse whose parse function returns the result of parserA's func if its not empty, 
+  -- or the result of parserB's func otherwise
+  -- Parser p <|> Parser k = Parser $ \str -> case p str of 
+  --                                          Just (x, restStr) -> Just (x, restStr)
+  --                                          Nothing           -> k str
+
+  Parser p <|> Parser k = Parser $ \str -> (p str) <|> (k str)
+
+
+-- psevdo kod
+-- oneOrMore p -> p + zeroOrMore p
+-- zeroOrMore -> empty | oneOrMore
 
 oneOrMore, zeroOrMore  :: Parser a -> Parser [a]
-oneOrMore = undefined
+-- try the parser once and then continue with zeroOrMore, all the while merging the results in a list using (:)
+oneOrMore parser = liftA2 (:) parser $ zeroOrMore parser
 
-zeroOrMore = undefined
+-- either we have 1 item (or more), or we need to wrap an empty list with a functor -> use 'pure'
+zeroOrMore parser = oneOrMore parser <|> pure []
+
